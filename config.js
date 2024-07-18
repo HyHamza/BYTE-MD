02032c2951954de19e451f106cad50e4d776f324
DJWAX <djmodzofcx@gmail.com>
2024-07-18 21:38:16 +0500

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ global.appUrl = process.env.APP_URL || "";
 global.email = "TalkDrove@gmail.com";
 global.location = "Lahore,Pakistan.";
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://Hamza:3800380ww@cluster0.uwommwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-global.allowJids = process.env.ALLOW_JID || "923072380380@s.whatsapp.net";
+global.allowJids = process.env.ALLOW_JID || "923427582273@s.whatsapp.net";
 global.blockJids = process.env.BLOCK_JID || "null";
 global.DATABASE_URL = process.env.DATABASE_URL || "";
 global.timezone = process.env.TZ || process.env.TIME_ZONE || "Asia/Karachi";
@@ -18,8 +18,8 @@ global.gurl = process.env.GURL || "https://whatsapp.com/channel/0029VaNRcHSJP219
 global.website = process.env.GURL || "https://whatsapp.com/channel/0029VaNRcHSJP2199iMQ4W0l";
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || process.env.IMAGE || "https://raw.githubusercontent.com/HyHamza/HyHamza/main/Images/logo.jpg";
 global.devs = "923072380380";
-global.sudo = process.env.SUDO || "923072380380";
-global.owner = process.env.OWNER_NUMBER || "923072380380";
+global.sudo = process.env.SUDO || "923427582273";
+global.owner = process.env.OWNER_NUMBER || "923427582273";
 global.style = process.env.STYLE || "3";
 global.gdbye = process.env.GOODBYE || "false";
 global.wlcm = process.env.WELCOME || "false";
@@ -42,7 +42,7 @@ global.scan = "https://byte-session.vercel.app/";
 
 global.SESSION_ID =
   process.env.SESSION_ID ||
-  "Your session ID" //Make sure session id starts with Byte;;;
+  "Byte;;;eyJub2lzZUtleSI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiRVA2N0FjdTdhbFVEdjJTODFHQTFUeTdXZnhoVjJUOEtWUHdEOU5paEIwRT0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiT2NKTlZTMGVQWCtjMlVHWGIxZ1VTb2pGRmJyZ1VKNlgxWFFDWlNQdzlGZz0ifX0sInBhaXJpbmdFcGhlbWVyYWxLZXlQYWlyIjp7InByaXZhdGUiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJPSzI2cHBCQStOczhQd0wrUzRJVVdaWlkvOXNldWgrRitEckc1RlFLVTFzPSJ9LCJwdWJsaWMiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJWeDFRRlBpWWxwaXNEM3VETDkrNTgzZU5ITnZwajBwbkZva3BTbys5Qm1FPSJ9fSwic2lnbmVkSWRlbnRpdHlLZXkiOnsicHJpdmF0ZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlFITWxTQkNndFcweURXcW51cHBuVHVFWE85YkY0VEkxL3EwWmd0UW4yM1k9In0sInB1YmxpYyI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6ImJUazdhN0c1dEFjcnNQTGtYQk5FNlV4Q05iVWVYTE5Wd0lpZGowdi9xbmc9In19LCJzaWduZWRQcmVLZXkiOnsia2V5UGFpciI6eyJwcml2YXRlIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoidUtBVE9Nay9Pc3RKWmNwQ3FEQmNvVW1xUVIrV3p3Zk1raG1iNXdvcnRVdz0ifSwicHVibGljIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiYldXamFrN1F1bUxCZjZ2aFYxLzFHUmJTeVUvQXBHeXc5RFVWZTc5S0QyYz0ifX0sInNpZ25hdHVyZSI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlNEOEtwYjYxTXFYVmdMQjYzWXF3MDBPSmtsQ2NSMStuTUszbDVQUUwwaHhidWVSRnRhQXp6bnJyTldYUmZZenBLZWRIOU53MWg1bWhOY2lmbXNCRGlnPT0ifSwia2V5SWQiOjF9LCJyZWdpc3RyYXRpb25JZCI6MCwiYWR2U2VjcmV0S2V5IjoienNia01GS1VVK3ZWYzZ6eHp6TGx1aEJxZ0htdHhqTW5UQm1IdCs2L2R6ST0iLCJwcm9jZXNzZWRIaXN0b3J5TWVzc2FnZXMiOltdLCJuZXh0UHJlS2V5SWQiOjMxLCJmaXJzdFVudXBsb2FkZWRQcmVLZXlJZCI6MzEsImFjY291bnRTeW5jQ291bnRlciI6MCwiYWNjb3VudFNldHRpbmdzIjp7InVuYXJjaGl2ZUNoYXRzIjpmYWxzZX0sImRldmljZUlkIjoiOVNjU0F5b1NUOXVUQlBGLTlqUmJWZyIsInBob25lSWQiOiIwNjcxZjYyNi04NjE5LTQzYmUtODlhMy1kZmFhNDI2NTY0NDQiLCJpZGVudGl0eUlkIjp7InR5cGUiOiJCdWZmZXIiLCJkYXRhIjoiekZEdTkwT2krVEVtWkh2cVBIOEY1cHo1WFVRPSJ9LCJyZWdpc3RlcmVkIjp0cnVlLCJiYWNrdXBUb2tlbiI6eyJ0eXBlIjoiQnVmZmVyIiwiZGF0YSI6IlVVMGQ3SnFjSkZMVEZvZFlvTjhVK0ZOelRKST0ifSwicmVnaXN0cmF0aW9uIjp7fSwicGFpcmluZ0NvZGUiOiJYUFdNMkRRUiIsIm1lIjp7ImlkIjoiOTIzNDI3NTgyMjczOjMyQHMud2hhdHNhcHAubmV0In0sImFjY291bnQiOnsiZGV0YWlscyI6IkNQU1JqOWtCRUxtSTViUUdHQUVnQUNnQSIsImFjY291bnRTaWduYXR1cmVLZXkiOiI5TXlmTi9CZ1JmVWx2d2NTenJwYVdDVE1BWXZsd3lLQmtZRzJtd1VqVEcwPSIsImFjY291bnRTaWduYXR1cmUiOiJwZ085WjB4bDZQTHlHQWUrd1ZKWDFwaWxpK1RiWENTQVlMa2dnM2NHTyt4SE1JZXJqRGpSa2Ryd1htdWRXWVZ0QmNycEk4czBYclQ2dzBiQ0QwZ0lDdz09IiwiZGV2aWNlU2lnbmF0dXJlIjoiUE9vOURjNTZMdVVHaTlseHJZa1NURGRoQUw2SEx2RDZzWXpCNnU1UXFwNjdLOExpZlcrRmhGYXVXTHQrNS80Rm5Lb0hMaFpJcldZUHJ3Y0o5dG1jaFE9PSJ9LCJzaWduYWxJZGVudGl0aWVzIjpbeyJpZGVudGlmaWVyIjp7Im5hbWUiOiI5MjM0Mjc1ODIyNzM6MzJAcy53aGF0c2FwcC5uZXQiLCJkZXZpY2VJZCI6MH0sImlkZW50aWZpZXJLZXkiOnsidHlwZSI6IkJ1ZmZlciIsImRhdGEiOiJCZlRNbnpmd1lFWDFKYjhIRXM2NldsZ2t6QUdMNWNNaWdaR0J0cHNGSTB4dCJ9fV0sInBsYXRmb3JtIjoiYW5kcm9pZCIsImxhc3RBY2NvdW50U3luY1RpbWVzdGFtcCI6MTcyMTMyMDUxOCwibXlBcHBTdGF0ZUtleUlkIjoiQUFBQUFFZEQifQ==" //Make sure session id starts with Byte;;;
 module.exports = {
   menu: process.env.MENU || "",
   HANDLERS: process.env.PREFIX || ",",
