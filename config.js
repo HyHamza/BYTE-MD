55a2f817182364a0754641687036f1e93175c5f9
Carlos Ke <167988058+Carlos2544@users.noreply.github.com>
2024-08-04 08:32:46 +0200

    Update config.js

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ global.read_status = process.env.AUTO_READ_STATUS || "true";
 global.save_status = process.env.AUTO_SAVE_STATUS || "false";
 global.save_status_from = process.env.SAVE_STATUS_FROM || "all";
 global.read_status_from = process.env.READ_STATUS_FROM || "all";
-global.auto_typing = process.ENV.AUTO_TYPING || "true";
+
 global.api_smd = "https://HyHamza.vercel.app";
 global.scan = "https://byte-session.vercel.app/";
 
